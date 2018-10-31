2e5e708f455fff25cd697304d0831abdef7292d6
Oleksandra MASLOVA <omaslova@e3r5p1.unit.ua>
2018-10-31 16:26:35 +0200

    skolko mozhno

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include "filler.h"
 
-// void	put_up(t_data *data)
-// {
-
-// }
-
-// void	algorithm(t_data *data)
-// {
-
-// }
-
 void	clear_matrix(t_data *data, int *counter)
 {
 	int i;
@@ -29,61 +19,31 @@ void	clear_matrix(t_data *data, int *counter)
 	*counter = 0;
 }
 
-// int		another_side(t_data *data)
-// {
-// 	int i;
-// 	int j;
-// 	int i_piece;
-// 	int change;
-
-// 	i = -1;
-// 	change = 0;
-// 	i_piece = 0;
-// 	data->y_piece -= 1;
-// 	dprintf(3, "THERE_2\n");
-// 	while (++i < data->row)
-// 	{
-// 		j = -1;
-// 		while (++j < data->column)
-// 		{
-// 			// dprintf(3, "data->matrix[i][j] = %c, j = %d    ", );
-// 			if (data->matrix[i][j] != data->player && data->matrix[i][j + 1] == data->player
-// 				&& j + 1 >= data->y_piece)
-// 			{
-// 				dprintf(3, "THERE_3\n");
-// 				data->X = i;
-// 				data->Y = j + 1;
-// 				while (data->piece[i_piece][data->y_piece--] == '*')
-// 					data->Y--;
-// 				change = 1;
-// 			}
-// 			// dprintf(3, "\n");
-// 		}
-// 	}
-// 	if (change == 0)
-// 		return (0);
-// 	return (1);
-// }
-
 int		change_coords(t_data *data)
 {
 	int i;
 	int j;
+
 	// int change;
 	// char enemy;
 
-	i = -1;
-	if (data->matrix[data->my_pos[0] + 1][data->my_pos[1]] == data->player)
+	i = data->X;
+	while (i < data->row)
 	{
-		while (++i < data->x_piece)
+		j = -1;
+		while (++j < data->column)
 		{
-			j = -1;
-			while (++j < data->y_piece)
-				if (j < data->y_piece - 1 && data->piece[i][j] == '*')
-					data->Y--;
-			if (i < data->x_piece - 1 && data->piece[i][j] == '*')
+			if (data->matrix[i + 1][j] == data->player && data->x_piece > 1)
+			{
+				dprintf(3, "i = %d, j = %d WTF\n", i, j);
 				data->X--;
-		}	
+			}
+			// if (j < data->y_piece && data->piece[i][j] == '*')
+			// 	data->Y--;
+		}
+		// if (i < data->x_piece && data->piece[i][j] == '*')
+		// 	data->X--;
+		i++;
 	}
 	return (1);
 }
@@ -92,39 +52,81 @@ void	define_coords(t_data *data)
 {
 	int i;
 	int j;
-	int i_piece;
-	int j_piece;
+	char enemy;
+	int flag;
 
-	i_piece = -1;
-	i = data->my_pos[0];
-	j = data->my_pos[1];
-	while (++i_piece < data->x_piece)
+	i = -1;
+	enemy = data->player == 'o' ? 'x' : 'o';
+	data->X = data->my_pos[0];
+	data->Y = data->my_pos[1];
+	dprintf(3, "data->X = %d, data->Y = %d\n", data->X, data->Y);
+	while (++i < data->x_piece)
 	{
-		j_piece = 0;
-		while (data->piece[i_piece][j_piece] == '.' && j_piece < data->y_piece)
-			j_piece++;
-		if (j_piece < data->y_piece)
+		j = -1;
+		flag = 0;
+		while (++j < data->y_piece && data->piece[i][j] == '.')
+		{
+			flag++;
+			data->Y--;
+		}
+		if (data->piece[i][j] == '*')
 			break ;
+		if (flag == j)
+		{
+			data->X--;
+			data->Y += flag - 1;
+		}
 	}
-	data->X = i - i_piece;
-	data->Y = j - j_piece;
-	// if (data->matrix[data->X][data->Y + 1] != '0' && data->Y != data->y_piece - 1)
-		// data->matrix[data->my_pos[2] - 1][data->my_pos[3]] - 48 < data->matrix[data->my_pos[0] + 1][data->my_pos[1]] - 48)
-	// {
-	// 	dprintf(3, "THERE\n");
-	// 	if (!change_coords(data))
-	// 	{
-	// 		data->X = 0;
-	// 		data->Y = 0;
-	// 	}
-	// }
-	if (data->X + data->x_piece > data->row)
+	dprintf(3, "data->X = %d, data->Y = %d\n", data->X, data->Y);
+	if (data->matrix[data->X + 1][data->Y] == data->player || data->matrix[data->X + 1][data->Y] == enemy)
 	{
-		dprintf(3, "RECURSION\n");
-		data->my_pos[0] = data->my_pos[2];
-		data->my_pos[1] = data->my_pos[3];
-		return (define_coords(data));
+		data->X -= data->x_piece - 1;
+		data->Y += flag - 1;
+		// while (data->matrix[data->X + 1][data->Y] != data->player)
+		// {
+		// 	data->Y--;
+		// 	dprintf(3, "THERE-\n");
+		// }
+		dprintf(3, "THERE+\n");
+		dprintf(3, "data->X = %d, data->Y = %d\n", data->X, data->Y);
+		while (data->matrix[data->X + data->x_piece - 1][data->Y + data->y_piece - 1] == data->player)
+		{
+			data->X--;
+		}
+		// if (data->x_piece > 1)
+		// 	data->my_pos[0]--;
+		// else
+		// 	return ;
+		// data->x_piece--;
 	}
+	// else if (data->matrix[data->X][data->Y + 1] == data->player || data->matrix[data->X][data->Y + 1] == enemy)
+	// {
+	// 	// i = 0;
+	// 	dprintf(3, "THERE_2\n");
+	// 	data->my_pos[1]--;
+	// 	// j++;
+	// 	// if (j == data->y_piece)
+	// 	// 	j = 0;
+	// }
+		// if (!change_coords(data))
+		// {
+		// 	data->X = 0;
+		// 	data->Y = 0;
+		// }
+	// if (flag == 0)
+	// {
+	// 	i = 0;
+	// 	j = 0;
+	// 	return ;
+	// }
+	// return (define_coords(data));
+	// if (data->X + data->x_piece > data->row)
+	// {
+	// 	dprintf(3, "RECURSION\n");
+	// 	data->my_pos[0] = data->my_pos[2];
+	// 	data->my_pos[1] = data->my_pos[3];
+	// 	return (define_coords(data));
+	// }
 }
 
 void	search_position(t_data *data)
@@ -132,20 +134,20 @@ void	search_position(t_data *data)
 	int i;
 	int j;
 	int first;
-	int the_best_pos[2];
+	int tbp[2]; // the best position to put the piece; (x, y) coordinates
 
 	first = 1;
 	i = data->my_pos[2];
-	the_best_pos[0] = data->my_pos[2];
-	the_best_pos[1] = data->my_pos[3];
+	tbp[0] = data->my_pos[2];
+	tbp[1] = data->my_pos[3];
 	dprintf(3, "data->my_pos[0] = %d\n", data->my_pos[0]);
 	dprintf(3, "data->my_pos[1] = %d\n", data->my_pos[1]);
 	dprintf(3, "data->my_pos[2] = %d\n", data->my_pos[2]);
 	dprintf(3, "data->my_pos[3] = %d\n", data->my_pos[3]);
-	dprintf(3, "the_best_pos[0] = %d  ", the_best_pos[0]);
-	dprintf(3, "the_best_pos[1] = %d\n", the_best_pos[1]);
-	dprintf(3, "data->matrix[the_best_pos[0]][the_best_pos[1]] = %c  ", data->matrix[the_best_pos[0]][the_best_pos[1]]);
-	dprintf(3, "data->matrix[the_best_pos[0]][the_best_pos[1]] = %d\n", data->matrix[the_best_pos[0]][the_best_pos[1]] - 48);
+	dprintf(3, "tbp[0] = %d  ", tbp[0]);
+	dprintf(3, "tbp[1] = %d\n", tbp[1]);
+	dprintf(3, "data->matrix[tbp[0]][tbp[1]] = %c  ", data->matrix[tbp[0]][tbp[1]]);
+	dprintf(3, "data->matrix[tbp[0]][tbp[1]] = %d\n", data->matrix[tbp[0]][tbp[1]] - 48);
 	while (i < data->row)
 	{
 		j = -1;
@@ -154,35 +156,42 @@ void	search_position(t_data *data)
 			while (data->matrix[i][j] == data->player && data->matrix[i][j + 1] == data->player)
 				j++;
 			if (data->matrix[i][j] == data->player && data->matrix[i][j + 1] != data->player && !first &&
-				data->matrix[i][j + 1] - 48 < data->matrix[the_best_pos[0]][the_best_pos[1] + 1] - 48)
+				data->matrix[i][j + 1] - 48 < data->matrix[tbp[0]][tbp[1] + 1] - 48)
 			{
 				// dprintf(3, "VIUVIU\n");
-				// dprintf(3, "data->matrix[the_best_pos[0]][the_best_pos[1]] = %c  ", data->matrix[the_best_pos[0]][the_best_pos[1]]);
-				// dprintf(3, "data->matrix[the_best_pos[0]][the_best_pos[1]] = %d\n", data->matrix[the_best_pos[0]][the_best_pos[1]] - 48);
+				// dprintf(3, "data->matrix[tbp[0]][tbp[1]] = %c  ", data->matrix[tbp[0]][tbp[1]]);
+				// dprintf(3, "data->matrix[tbp[0]][tbp[1]] = %d\n", data->matrix[tbp[0]][tbp[1]] - 48);
 				// dprintf(3, "i = %d, j = %d\n", i, j);
-				the_best_pos[0] = i;
-				the_best_pos[1] = j;
+				tbp[0] = i;
+				tbp[1] = j;
 			}
 			else if (data->matrix[i][j] == data->player && data->matrix[i][j + 1] != data->player && first)
 			{
 				first = 0;
-				the_best_pos[0] = i;
-				the_best_pos[1] = j;
+				tbp[0] = i;
+				tbp[1] = j;
 			}
 		}
-		// dprintf(3, "the_best_pos[0] = %d  ", the_best_pos[0]);
-		// dprintf(3, "the_best_pos[1] = %d\n", the_best_pos[1]);
+		// dprintf(3, "tbp[0] = %d  ", tbp[0]);
+		// dprintf(3, "tbp[1] = %d\n", tbp[1]);
 		i++;
 	}
-	if (the_best_pos[0] == data->my_pos[2] && the_best_pos[1] == data->my_pos[3])
+	if (tbp[0] == data->my_pos[2] && tbp[1] == data->my_pos[3])
 	{
-		the_best_pos[0] = data->my_pos[0];
-		the_best_pos[1] = data->my_pos[1];
+		tbp[0] = data->my_pos[0];
+		tbp[1] = data->my_pos[1];
 	}
-	dprintf(3, "the_best_pos[0] = %d\n", the_best_pos[0]);
-	dprintf(3, "the_best_pos[1] = %d\n", the_best_pos[1]);
-	data->my_pos[0] = the_best_pos[0];
-	data->my_pos[1] = the_best_pos[1];
+	if (tbp[0] + data->x_piece > data->row || tbp[1] + data->y_piece > data->column)
+	{
+		tbp[0] = data->my_pos[2];
+		tbp[1] = data->my_pos[3];
+	}
+	// if (data->matrix[tbp[0] + 1][tbp[1]] == 111 || data->matrix[tbp[0]][tbp[1] + 1] == 111)
+		
+	dprintf(3, "tbp[0] = %d\n", tbp[0]);
+	dprintf(3, "tbp[1] = %d\n", tbp[1]);
+	data->my_pos[0] = tbp[0];
+	data->my_pos[1] = tbp[1];
 }
 
 void	set_distance(t_data *data)
@@ -342,11 +351,6 @@ void	get_data(char *line, t_data *data, int fd)
 	str1 = ft_itoa(data->X);
 	str2 = ft_itoa(data->Y);
 	dprintf(3, "%d %d\n", data->X, data->Y);
-	// printf("%d %d\n", data->my_pos[0], data->my_pos[1]);
-	// ft_putnbr(data->my_pos[1]);
-	// ft_putchar(' ');
-	// ft_putnbr(data->my_pos[0]);
-	// ft_putchar('\n');
 	str1 = ft_strcat(str1, " ");
 	str2 = ft_strcat(str2, "\n");
 	ft_putstr(ft_strcat(str1, str2));
@@ -362,24 +366,12 @@ int main(void)
 	t_data		*data;
 
 	data = (t_data *)malloc(sizeof(t_data));
-	// fd = open("test", O_RDWR | O_TRUNC | O_CREAT);
-	// printf("%d\n", fd);
-	// while (get_next_line(0, &line))
-	// {
-	// 	dprintf(fd, "%s\n", line);
-	// 	free(line);
-	// }
-	// close(fd);
-	// fd = open("test", O_RDONLY);
 	fd = open("log", O_RDWR | O_CREAT);
 	i = 0;
 	while (get_next_line(0, &line))
 	{
 		if ((ptr = ft_strstr(line, "[./a.out]")))
-		{	
-			// dprintf(3, "ptr = %sn", ptr - 4);
 			data->player = *(ptr - 4) == '1' ? 'o' : 'x';
-		}
 		if ((ptr = ft_strstr(line, "Plateau")))
 			set_data(line, data);	
 		if ((ptr = ft_strstr(line, "Piece")))
